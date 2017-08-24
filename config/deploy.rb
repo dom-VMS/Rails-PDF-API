bd1b3f4995cade42df25f54adc99dfb63eee2f4a
Toby Varland <toby@tobyvarland.com>
2017-08-24 13:51:45 -0400

    Update Capistrano Options

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "https://github.com/VarlandMetalService/Rails-PDF-API.git"
 
 set :deploy_to, '/home/vcms/Rails-PDF-API'
 
-append :linked_files, "config/database.yml", "config/secrets.yml"
+# append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default branch is :master
