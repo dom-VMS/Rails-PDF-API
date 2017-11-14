d3bca27816c1c7bef2a461bd38d44b0ab39020ce
Toby Varland <toby@tobyvarland.com>
2017-11-14 15:05:45 -0500

    Update Capistrano Version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.9.0"
+lock "3.10.0"
 
 set :application, "Rails-PDF-API"
 set :repo_url, "https://github.com/VarlandMetalService/Rails-PDF-API.git"
